386467457d923f0f8bbe46f25ae461e2c7f94518
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2020-12-05 16:28:37 +0100

    Initial commit

---
+++ desktop/src/com/asodc/test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asodc.test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asodc.test.LibgdxHelloWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibgdxHelloWorld(), config);
+	}
+}
