a7071abdbecbf3ed3590171aa859a6b45d2eace0
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2020-12-06 12:09:21 +0100

    edit comments

--- desktop/src/com/asodc/test/desktop/DesktopLauncher.java
+++ desktop/src/com/asodc/test/desktop/DesktopLauncher.java
@@ -19,9 +19,6 @@ public class DesktopLauncher {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
         // fields are public so I assume the idea is to manupulate them...
-        // fullscreen seems to fuck the ability to click on other open apps in Pop!_OS, even when alt-tabbed...
-        // I have a feeling this isn't just limited to libgdx as I think Steam Proton has a similar issue
-        config.fullscreen = false; // default is false, but whatever
 
         // fairly self-explanitory
 //		config.addIcon();
@@ -29,16 +26,16 @@ public class DesktopLauncher {
         // returns an array of the supported display modes
 //		Graphics.DisplayMode[] displayModes = config.getDisplayModes();
         // the returned object is static, so should probably be doing this?
-        Graphics.DisplayMode[] displayModes = LwjglApplicationConfiguration.getDisplayModes();
+//        Graphics.DisplayMode[] displayModes = LwjglApplicationConfiguration.getDisplayModes();
 
         // gets the currently configured display mode
 //		Graphics.DisplayMode displayMode = config.getDesktopDisplayMode();
         // the returned object is static, so should probably be doing this?
-        Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+//        Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
 
         // makes sense that you would plop one of the display modes from above in here
 		// I guess the idea is to allow a user to configure the application?
-        config.setFromDisplayMode(displayMode);
+//        config.setFromDisplayMode(displayMode);
 
         // ***** LwjglApplication *****
         // The auto-generated code didn't initialise an object reference
